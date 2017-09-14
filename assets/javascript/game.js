c77a9b5d64a9ea461f8c0fbf1c5b7858bcdc393d
Brian Jin <brnjin6@gmail.com>
2017-09-14 16:34:28 -0400

    guesses left working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,11 +3,11 @@ var wins = document.getElementById("numberOfWins");
 var loss = document.getElementById("numberOfLosses");
 var guesses = document.getElementById("numberOfguesses");
 var lettersGuessed = "";
-
+var guesses = 13;
 
 
 /*Array of words*/	
-var memeNames = ["pepe", "coldsteel", "doge", "grumpy cat", "success kid", "peanut butter kid"];
+var memeNames = ["blue", "red", "green", "purple", "grey", "white", "black", "orange"];
 
 var randoms = memeNames[Math.floor(Math.random() * memeNames.length)];
 /*Resets the game and keeps score*/
@@ -15,7 +15,6 @@ window.onload = function startUp () {
 	var answerWord = randoms;
 	var wins = 0;
 	var loss = 0;
-	var guesses = 13;
 /*Grabs random meme name*/
 var giveMeWords = blankTranslator(randoms);
 
@@ -40,44 +39,12 @@ document.onkeyup = function(event) {
 	var userGuess = event.key;
 
 	/*Remember what key is pressed*/
-		if (userGuess !== lettersGuessed){
-			document.getElementById("Letters-guessed").innerHTML = "Letters Guessed: " + (lettersGuessed = lettersGuessed + userGuess);
-		};
-
-	}; 	
-
-
-// win counter
-/*if (currentWord === "pepe") {
-	wins = wins + 1
-	document.write (wins)
-else (guesses -> 0) {
-};
-if (currentWord === "coldsteel") {
-	wins = win + 1
-else (guesses -> 0) {
-};
-if (currentWord === "doge") {
-	wins = win + 1
-else (guesses -> 0) {
-};
-if (currentWord === "grumpy cat") {
-	wins = win + 1
-else (guesses -> 0) {
-};
-if (currentWord === "peanut butter kid") {
-	wins = win + 1
-else (guesses -> 0) {
-};
-if (currentWord === "success kid") {
-	wins = win + 1
-else (guesses -> 0) {
-};*/
-/*winCount = getElementById("numberOfWins")
-*/
-
-// Letters guessed 
-// Current word 
-// number of guesses remaining 
-
-
+if (guesses > 0){
+    document.getElementById("Letters-guessed").innerHTML = "Letters Guessed: " + (lettersGuessed = lettersGuessed + userGuess);
+    guesses -= 1;
+    };
+    if (guesses === 0){
+    	alert("You blew it man! You blew it!");
+    }
+    document.getElementById('numberOfGuesses').textContent = "Number of Guesses Left: " + guesses
+	}; 
